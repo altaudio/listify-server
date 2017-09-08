6cef5375c9c51038c26966f830cde1bd8b2b0624
Richard Gill <richard@rgill.co.uk>
2017-09-08 16:53:13 +0100

    Adds HTTPS request handling and firebase dev and prod

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -1,1 +1,9 @@
-export default {}
+export default {
+  firebase: {
+    apiKey: 'AIzaSyBuKnRIy1PuMQ6IHJ__-myOF8CSKjl3Bkg',
+    authDomain: 'listify-38525.firebaseapp.com',
+    databaseURL: 'https://listify-38525.firebaseio.com',
+    projectId: 'listify-38525',
+    messagingSenderId: '934259624713',
+  }
+}
--- src/config/development.js
+++ src/config/development.js
@@ -1,3 +1,9 @@
 export default {
-  firebaseUrl: 'https://hacker-how-dev.firebaseio.com',
+  firebase: {
+    apiKey: 'AIzaSyBuKnRIy1PuMQ6IHJ__-myOF8CSKjl3Bkg',
+    authDomain: 'listify-38525.firebaseapp.com',
+    databaseURL: 'https://listify-38525.firebaseio.com',
+    projectId: 'listify-38525',
+    messagingSenderId: '934259624713',
+  }
 }
