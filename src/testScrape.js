4f88f19ae89630a7caecb8bb84f0a9b7eeaa3541
Richard Gill <richard@rgill.co.uk>
2017-09-15 16:41:19 +0100

    Adds stations and test scrape function

---
+++ src/testScrape.js
@@ -0,0 +1,12 @@
+import scraperjs from 'scraperjs'
+
+scraperjs.StaticScraper.create('https://www.reddit.com/r/listentothis/new/')
+  .scrape(($) => {
+    return {
+      title: $('.entry .title .may-blank').first().text(),
+      artist: $('.entry .title .may-blank').first().text(),
+    }
+  })
+  .then((track) => {
+    console.log(track)
+  })
